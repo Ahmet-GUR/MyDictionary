8474a826dde228819402b8c0d1d624733333079e
A.G <A.G@ZENOPANELLTDT>
2021-01-23 14:19:39 +0300

    Add project files.

---
+++ MyDictionary/MyDictionary.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyDictionary
+{
+    class MyDictionary<TKey, TValue>
+    {
+        TKey[] _keys;
+        TValue[] _values;
+        TKey[] _tempKeys;
+        TValue[] _tempValue;
+
+        public MyDictionary()
+        {
+            _keys = new TKey[0];
+            _values = new TValue[0];
+        }
+
+        public void Add(TKey key, TValue value)
+        {
+            _tempKeys = _keys;
+            _tempValue = _values;
+            _keys = new TKey[_keys.Length + 1];
+            _values = new TValue[_keys.Length + 1];
+            for (int i = 0; i < _tempKeys.Length; i++)
+            {
+                _keys[i] = _tempKeys[i];
+                _values[i] = _tempValue[i];
+            }
+            _keys[_keys.Length - 1] = key;
+            _values[_values.Length - 1] = value;
+
+        }
+
+        public int Count
+        {
+            get { return _keys.Length; }
+        }
+
+    }
+}
---
+++ MyDictionary/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<int, string> students = new MyDictionary<int, string>();
+            students.Add(1, "Ahmet GÜR");
+            students.Add(2, "Engin DEMİROĞ");
+            students.Add(3, "Berkay BİLGİN");
+            students.Add(4, "Eda KAVANOZ");
+            students.Add(5, "Ayşe YURTSEVER");
+
+            
+            Console.WriteLine(students.Count);
+            Console.ReadLine();
+            
+        }
+    }
+}
